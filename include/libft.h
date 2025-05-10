b37d8148ae1ac98b42337d049e7d00207c0fdad4
Santiago Gomez Mesa <sgomez-m@c2r10s2.42malaga.com>
2025-05-10 23:06:04 +0200

    pendiente corregir substr

--- include/libft.h
+++ include/libft.h
@@ -6,7 +6,7 @@
 /*   By: sgomez-m <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/06 13:31:10 by sgomez-m          #+#    #+#             */
-/*   Updated: 2025/05/09 14:06:00 by sgomez-m         ###   ########.fr       */
+/*   Updated: 2025/05/10 20:12:07 by sgomez-m         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,13 +15,17 @@
 # include <ctype.h>
 # include <stddef.h>
 # include <stdio.h>
+# include <stdlib.h>
 
 //importing functions
 
 char	*ft_strchr(const char *s, int c);
 char	*ft_strrchr(const char *s, int c);
+char	*ft_strdup(const char *s);
 void	*ft_memset(void *s, int c, size_t n);
+void	*ft_calloc(size_t nmemb, size_t size);
 void	ft_bzero(void *s, size_t n);
+void	*ft_calloc(size_t nmemb, size_t size);
 void	*ft_memchr(const void *s, int c, size_t n);
 int		ft_strncmp(const char *s1, const char *s2, size_t n);
 int		ft_isalpha(int c);
@@ -36,6 +40,7 @@ void	*ft_memcpy(void *dest, const void *src, size_t n);
 int		ft_memcmp(const void *s1, const void *s2, size_t n);
 void	*ft_memmove(void *dest, const void *src, size_t n);
 char	*ft_strnstr(const char *big, const char *little, size_t len);
+char	*ft_substr(char const *s, unsigned int start, size_t len);
 size_t	ft_strlcpy(char *dest, char *src, size_t size);
 size_t	ft_strlcat(char *dst, const char *src, size_t size);
 size_t	ft_strlen(const char *str);
